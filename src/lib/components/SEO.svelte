1b7c6ea5fe5cfb08493a5d0f3e6fdcfd2c7e9506
Irvan Malik <irvanmalik48@gmail.com>
2022-10-12 19:02:35 +0700

    feat(lib/components): add navbar and seo components
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+  export let keywords: string;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+  <meta name="keywords" content={keywords} />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta httpequiv="X-UA-Compatible" content="ie=edge" />
+</svelte:head>
